42d986f268c86a140d487a14991afbc996032680
IAmREGE <junyu336699@sina.com.cn>
2024-12-15 09:02:48 +0000

    0.0.1-a.1: Develop

--- 1/src/main/java/rege/rege/statesrestorer/util/BiShortData.java
+++ 1/src/main/java/rege/rege/statesrestorer/util/BiShortData.java
@@ -15,7 +15,33 @@ public class BiShortData implements LeveledData<Boolean, Short> {
         return isAddi.booleanValue() ? this.addi : this.main;
     }
 
+    public boolean isEmpty() {
+        return this.main == null && this.addi == null;
+    }
+
     public byte[] serialize(Object... args) {
         return new byte[0];
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof BiShortData)) {
+            return false;
+        }
+        BiShortData that = (BiShortData)o;
+        return (this.main == null ? that.main == null :
+                this.main.equals(that.main)) &&
+               (this.addi == null ? that.addi == null :
+                this.addi.equals(that.addi));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = this.main != null ? this.main.hashCode() : 0;
+        result = 31 * result + (this.addi != null ? this.addi.hashCode() : 0);
+        return result;
     }
 }
